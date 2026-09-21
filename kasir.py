f0ec9494f022f0d4b019a0f8ca875a43b9632c4b
Kedai Cihuuuy <kedaicihuuuy24@gmail.com>
2026-09-21 10:26:50 +0700

    Menambahkan pilihan keluar

--- kasir.py
+++ kasir.py
@@ -1,41 +1,61 @@
-print("=== KASIR KEDAI ===")
-print("1. Seblak       Rp10.000")
-print("2. Mie Nyemek   Rp10.000")
-print("3. Baso Mercon  Rp10.000")
+from datetime import datetime
 
-pilihan = input("Pilih menu: ")
-jumlah = int(input("Jumlah: "))
+while True:
+    print()
+    print("=== KASIR KEDAI ===")
+    print("1. Seblak       Rp10.000")
+    print("2. Mie Nyemek   Rp10.000")
+    print("3. Baso Mercon  Rp10.000")
+    print("0. Keluar")
 
-if pilihan == "1":
-    menu = "Seblak"
-    harga = 10000
-elif pilihan == "2":
-    menu = "Mie Nyemek"
-    harga = 10000
-elif pilihan == "3":
-    menu = "Baso Mercon"
-    harga = 10000
-else:
-    print("Menu tidak tersedia")
-    exit()
+    pilihan = input("Pilih menu: ")
 
-total = harga * jumlah
+    if pilihan == "0":
+        print("Terima kasih!")
+        break
 
-print()
-print("Total belanja: Rp", total)
+    if pilihan == "1":
+        menu = "Seblak"
+        harga = 10000
+    elif pilihan == "2":
+        menu = "Mie Nyemek"
+        harga = 10000
+    elif pilihan == "3":
+        menu = "Baso Mercon"
+        harga = 10000
+    else:
+        print("Menu tidak tersedia!")
+        continue
 
-bayar = int(input("Uang bayar: Rp "))
-
-if bayar < total:
-    print("Uang tidak cukup!")
-    print("Kekurangan: Rp", total - bayar)
-else:
-    kembalian = bayar - total
+    jumlah = int(input("Jumlah: "))
+    total = harga * jumlah
 
     print()
     print("=== STRUK ===")
-    print("Menu      :", menu)
-    print("Jumlah    :", jumlah)
-    print("Total     : Rp", total)
-    print("Uang bayar: Rp", bayar)
-    print("Kembalian : Rp", kembalian)
+    print("Menu   :", menu)
+    print("Jumlah :", jumlah)
+    print("Total  : Rp", total)
+
+    bayar = int(input("Uang bayar: Rp "))
+
+    if bayar < total:
+        print("Uang tidak cukup!")
+        print("Kekurangan: Rp", total - bayar)
+        continue
+
+    kembalian = bayar - total
+    print("Kembalian: Rp", kembalian)
+
+    waktu = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
+    with open("transaksi.txt", "a") as file:
+        file.write("================================\n")
+        file.write("Waktu     : " + waktu + "\n")
+        file.write("Menu      : " + menu + "\n")
+        file.write("Jumlah    : " + str(jumlah) + "\n")
+        file.write("Total     : Rp " + str(total) + "\n")
+        file.write("Uang bayar: Rp " + str(bayar) + "\n")
+        file.write("Kembalian : Rp " + str(kembalian) + "\n")
+        file.write("================================\n\n")
+
+    print("Transaksi berhasil disimpan.")
